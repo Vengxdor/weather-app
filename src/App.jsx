5fc3ca18531b7c6d543495842755c054e331efef
Vengxdor <122954695+Vengxdor@users.noreply.github.com>
2023-09-15 17:28:05 +0000

    cosa

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import './App.css'
 
 function App() {
-  const video = '../background-video.mp4'
   
   return (
     <>
-    <div className="main">
-      
-    <h1>Steven Fernandez</h1>
-    </div>
-    {
-      video 
-      ? <div>
-      <video className='background'  autoPlay loop >
-        <source src={video} type='video/mp4' />
-      </video>
-      <div className="background-opacity"></div>
-    </div>
-    : <h1>Loading</h1> 
-    }
-    
+			    
     </>
   )
 }
